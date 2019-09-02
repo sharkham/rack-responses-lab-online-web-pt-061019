581ad16c385027000f5e8bb96f6f9eedca1a8f4d
Sam Markham <sam.e.markham@gmail.com>
2019-09-02 23:49:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
     resp = Rack::Response.new
       time = Time.new
 
-      if time.hour < 12
+      if time.hour == 9
         resp.write "Good Morning"
       elsif time.hour > 12
         resp.write "Good Afternoon"
