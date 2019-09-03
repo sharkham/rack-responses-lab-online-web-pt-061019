3eebff25418ac219899ed1ede6541a586148a967
Sam Markham <sam.e.markham@gmail.com>
2019-09-03 00:00:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,12 @@
+require 'pry'
+
 class Application
   def call(env)
     resp = Rack::Response.new
       time = Time.new
 
       if time.hour < 12
+        binding.pry
         resp.write "Good Morning"
       elsif time.hour > 12
         resp.write "Good Afternoon"
