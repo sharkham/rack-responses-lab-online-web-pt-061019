e56d3f9f4c22b7820e965fdc1118f1acd8ced644
Sam Markham <sam.e.markham@gmail.com>
2019-09-03 00:06:59 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
       time = Time.now
 
       if time.hour < 12
-        binding.pry
+
         resp.write "Good Morning"
       elsif time.hour > 12
         resp.write "Good Afternoon"
